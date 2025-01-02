1170244fbde604bca72de3a80c04f8bc57f1ce85
juliomr21 <juliomr21.correo@gmail.com>
2025-01-02 15:45:19 -0300

    pedidos y gastos para cliente

--- models/Client.js
+++ models/Client.js
@@ -4,6 +4,8 @@ const clientSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String },
+  pedidos: { type: Number, default: 0 }, // Campo para contar pedidos
+  gastoTotal: { type: Number, default: 0 }, // Campo para el gasto total
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true } // Relación con el usuario
 });
 
